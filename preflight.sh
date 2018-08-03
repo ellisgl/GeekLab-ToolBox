cb4ba42dd2d141aca03da851adf13ee576e2b0f5
ellisgl <xelliss@gmail.com>
2018-08-03 07:40:16 -0500

    Travis-CI stuff.

--- preflight.sh
+++ preflight.sh
@@ -16,8 +16,7 @@ done
 if [[ $ONLY_READMES == True ]]; then
   echo "Only .md files found, exiting."
   travis_terminate 0
-  exit 1
+  #exit 1
 else
   echo "Non-.md files found, continuing with build."
-  python verify_files.py
 fi
